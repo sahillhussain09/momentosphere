ad872ae7e7696643a89c9ddf9b3575eb6da83ef1
sahillhussain09 <sh7389764@email.com>
2023-10-25 18:12:10 +0530

    fix buggit add    frontend/src/components/create/Uploder.jsx

--- frontend/src/components/create/Uploder.jsx
+++ frontend/src/components/create/Uploder.jsx
@@ -13,6 +13,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { loadUser } from '../../redux/actions/UserActions';
 import "../../styles/Upload.css"
 import { postUpload } from '../../redux/actions/PostActions';
+import { Typography } from '@mui/material';
 
 
 const Transition = React.forwardRef(function Transition(props, ref) {
@@ -26,19 +27,17 @@ export default function AlertDialogSlide({ open, setOpen }) {
     const { loading, data, error } = useSelector((state) => state.postUpload);
 
 
-    useEffect(() => {
-        loadMyProfile(loadUser())
-    }, [])
-
-
     const [uploadFile, setUploadFile] = useState(null);
     const [caption, setCaption] = useState('')
+    const [postCreated, setPostCreated] = useState(data && data.success ? true : false)
 
 
     const handleClose = () => {
+        loadMyProfile(loadUser())
         setOpen(false);
         setUploadFile(null)
         setCaption("")
+        setPostCreated(false);
     };
 
     const handleFile = (event) => {
@@ -70,8 +69,6 @@ export default function AlertDialogSlide({ open, setOpen }) {
             // console.log("formData", formdata);
 
             uploadDispatch(postUpload(formdata))
-            loadMyProfile(loadUser());
-
         }
     }
 
@@ -99,35 +96,39 @@ export default function AlertDialogSlide({ open, setOpen }) {
 
             >
                 <DialogContent>
-                    <div className='upload-cont'>
-                        <div className='uploader-head'>
-                            {
-                                uploadFile ? <img alt='Oops! something bad' src={uploadFile} /> : <Button component="label" variant="contained" startIcon={<CloudUploadIcon />}>
-                                    Choose file
-                                    <VisuallyHiddenInput type="file" onChange={handleFile} />
-                                </Button>
-                            }
-                        </div>
+                    {
+                        !postCreated ? <div className='upload-cont'>
+                            <div className='uploader-head'>
+                                {
+                                    uploadFile ? <img alt='Oops! something bad' src={uploadFile} /> : <Button component="label" variant="contained" startIcon={<CloudUploadIcon />}>
+                                        Choose file
+                                        <VisuallyHiddenInput type="file" onChange={handleFile} />
+                                    </Button>
+                                }
+                            </div>
 
-                        <Box
-                            component="form"
-                            sx={{
-                                '& > :not(style)': { m: 1, width: '100%' },
+                            <Box
+                                component="form"
+                                sx={{
+                                    '& > :not(style)': { m: 1, width: '100%' },
 
-                            }}
-                            noValidate
-                            autoComplete="off"
-                        >
-                            <TextField id="standard-basic" label="Write caption here..." value={caption} variant="standard" onChange={(e) => setCaption(e.target.value)} />
-                        </Box>
+                                }}
+                                noValidate
+                                autoComplete="off"
+                            >
+                                <TextField id="standard-basic" label="Write caption here..." value={caption} variant="standard" onChange={(e) => setCaption(e.target.value)} />
+                            </Box>
 
-                    </div>
+                        </div> : <div className='upload-cont'> <Typography variant='h3'>{data && data.message}</Typography> </div>
+                    }
                 </DialogContent>
                 <DialogActions>
                     <Button size='small' color='error' onClick={handleClose}>Cancel</Button>
-                    <Button size='small' variant="contained" color="success" onClick={handleUpload}>
-                        {loading ? "loading..." : "upload"}
-                    </Button>
+                    {
+                        <Button size='small' variant="contained" color="success" onClick={handleUpload}>
+                            {loading ? "uploading..." : "upload"}
+                        </Button>
+                    }
                 </DialogActions>
             </Dialog>
         </form >
